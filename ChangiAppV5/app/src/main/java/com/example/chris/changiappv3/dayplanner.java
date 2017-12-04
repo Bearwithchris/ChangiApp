739033d47af845ee037771b62a2e2f91c8b241db
Bearwithchris <Christopher_teo@mymail.sutd.edu.sg>
2017-12-04 16:11:53 +0800

    v5
    1)@Xuan(sutd-climbers)  i made the scroll view so u can spread out ur data more, can standardise with emund or smth
    
    2)@edmund (sutd-istd)  bugs
    -Cannot add marina bays sand
    -Cannot clear
    But generally the algo is very good i can load 9 units and it runs super fast
    
    3)new format is after 5 items, we will automatically trigger fast algo
    
    1 more thing dont forget to use the actually budget indicated
    
    Updated version is called changiAppV5

--- ChangiAppV5/app/src/main/java/com/example/chris/changiappv3/dayplanner.java
+++ ChangiAppV5/app/src/main/java/com/example/chris/changiappv3/dayplanner.java
@@ -19,6 +19,7 @@ import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -50,29 +51,28 @@ public class dayplanner  extends FragmentActivity implements OnMapReadyCallback 
     float budget;
     ArrayList<Integer> list;
 
-    String updatedData;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dayplanner);
-        context=this;
+        context = this;
 
-        list=new ArrayList<>();
-        locationDbHelper=new LocationDbHelper(this);
-        locationDb=locationDbHelper.getWritableDatabase();
-        testview=findViewById(R.id.test);
+        list = new ArrayList<>();
+        locationDbHelper = new LocationDbHelper(this);
+        locationDb = locationDbHelper.getWritableDatabase();
+        testview = findViewById(R.id.test);
         testview.setMovementMethod(new ScrollingMovementMethod());
 
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
-        MapsSearch=findViewById(R.id.search);
-        navigate=findViewById(R.id.NavigateButton);
+        MapsSearch = findViewById(R.id.search);
+        navigate = findViewById(R.id.NavigateButton);
 
-        Intent intentExtras=getIntent();
-        algo=intentExtras.getBooleanExtra("algo",false);
-        budget=intentExtras.getFloatExtra("budget",20);
+        Intent intentExtras = getIntent();
+        algo = intentExtras.getBooleanExtra("algo", false);
+        budget = intentExtras.getFloatExtra("budget", 20);
         System.out.println(budget);
 
         onClickGetEntireDb();
@@ -80,41 +80,41 @@ public class dayplanner  extends FragmentActivity implements OnMapReadyCallback 
 
     }
 
-    public void onClickGetEntireDb(){
+    public void onClickGetEntireDb() {
 
         //TO DO 3.7 Call the query or rawQuery method of the spendingDb object and store the result in a Cursor object
-        final String SQL_QUERY_TABLE="SELECT * FROM "+LocationsContract.LocationEntry.TABLE_NAME;
+        final String SQL_QUERY_TABLE = "SELECT * FROM " + LocationsContract.LocationEntry.TABLE_NAME;
 
         //TO DO 3.8 Extract the data from the Cursor object and display it on the textView widget
         Cursor cursor = locationDb.rawQuery(SQL_QUERY_TABLE, null);
 
-        String outstring="";
-        int indexRemarks=cursor.getColumnIndex(LocationsContract.LocationEntry.COL_LOCATIONNAME);
-        int indexAmount=cursor.getColumnIndex(LocationsContract.LocationEntry.COL_AMOUNT);
-        int counter=0;
-        while(cursor.moveToNext()){
+        String outstring = "";
+        int indexRemarks = cursor.getColumnIndex(LocationsContract.LocationEntry.COL_LOCATIONNAME);
+        int indexAmount = cursor.getColumnIndex(LocationsContract.LocationEntry.COL_AMOUNT);
+        int counter = 0;
+        while (cursor.moveToNext()) {
             String myAmount = cursor.getString(indexAmount);
             String location = cursor.getString(indexRemarks);
 //            addRg(location,counter,Integer.parseInt(myAmount));
 //            counter+=1;
-           // System.out.println(location);
+            // System.out.println(location);
             convert(location);
-            outstring=outstring+location+" $"+myAmount+"\n";
+            outstring = outstring + location + " $" + myAmount + "\n";
         }
-
-        if(list.isEmpty()) {
+        if (list.isEmpty()) {
             testview.setText("No plans has been added");
         } else {
-            new AsyncFetch().execute("test");
-
+            new AsyncFetch().execute();
         }
+
 
     }
 
-    public void removeEntireDb(View view){
+    public void removeEntireDb(View view) {
         list.clear();
-        locationDb.delete(LocationsContract.LocationEntry.TABLE_NAME,null,null);
+        locationDb.delete(LocationsContract.LocationEntry.TABLE_NAME, null, null);
         onClickGetEntireDb();
+      //  AsyncFetch.
     }
 
     @Override
@@ -129,32 +129,33 @@ public class dayplanner  extends FragmentActivity implements OnMapReadyCallback 
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 
-    public void MapsSearch(View view){
+    public void MapsSearch(View view) {
 
         Geocoder geocoder;
         List<Address> addresses;
 
         geocoder = new Geocoder(this, Locale.getDefault());
 
-        try{
+        try {
 
-            String locationsearch=MapsSearch.getText().toString();
+            String locationsearch = MapsSearch.getText().toString();
 
-            addresses=geocoder.getFromLocationName(locationsearch+" singappore",1);
-            double latitude=addresses.get(0).getLatitude();
-            double longitude=addresses.get(0).getLongitude();
-            LatLng changiAirport=new LatLng(latitude,longitude);
+            addresses = geocoder.getFromLocationName(locationsearch + " singappore", 1);
+            double latitude = addresses.get(0).getLatitude();
+            double longitude = addresses.get(0).getLongitude();
+            LatLng changiAirport = new LatLng(latitude, longitude);
             marker.setPosition(changiAirport);
             mMap.moveCamera(CameraUpdateFactory.newLatLng(changiAirport));
             mMap.moveCamera(CameraUpdateFactory.zoomTo(14));
             marker.setTitle(locationsearch);
 
-        }catch(Exception ex){
+        } catch (Exception ex) {
             ex.printStackTrace();
         }
 
     }
-//    public void addRg(String location,int id,int Amount){
+
+    //    public void addRg(String location,int id,int Amount){
 //        RadioGroup rg = (RadioGroup) findViewById(R.id.rg);
 //
 //        RadioButton radioButton = new RadioButton(this);
@@ -163,75 +164,88 @@ public class dayplanner  extends FragmentActivity implements OnMapReadyCallback 
 //        RadioGroup.LayoutParams params = new RadioGroup.LayoutParams(RadioGroup.LayoutParams.WRAP_CONTENT, RadioGroup.LayoutParams.WRAP_CONTENT);
 //        rg.addView(radioButton, params);
 //    }
-    public void convert(String location){
+    public void convert(String location) {
         System.out.println(location);
-        if(location.equals("Marina Bay Sands")){
+        if (location.equals("Marina Bay Sands")) {
             list.add(0);
-        }
-        else if(location.equals("Singapore Flyer")){
+        } else if (location.equals("Singapore Flyer")) {
             list.add(1);
-        }
-        else if(location.equals("Vivo City")){
+        } else if (location.equals("Vivo City")) {
             list.add(2);
-        }
-        else if(location.equals("Universal Studios")){
+        } else if (location.equals("Universal Studios")) {
             list.add(3);
-        }
-        else if(location.equals("Buddha Tooth Relic Temple")){
+        } else if (location.equals("Buddha Tooth Relic Temple")) {
             list.add(4);
-        }
-        else if(location.equals("Singapore Zoo")){
+        } else if (location.equals("Singapore Zoo")) {
             list.add(5);
-        }
-        else if(location.equals("Orchard Road")){
+        } else if (location.equals("Orchard Road")) {
             list.add(6);
-        }
-        else if(location.equals("Jurong Bird Park")){
+        } else if (location.equals("Jurong Bird Park")) {
             list.add(7);
-        }
-        else if(location.equals("Singapore Botanic Gardens")){
+        } else if (location.equals("Singapore Botanic Gardens")) {
             list.add(8);
-        }
-        else if(location.equals("SUTD")){
+        } else if (location.equals("SUTD")) {
             list.add(9);
         }
 
 
     }
-    public void print(){
-        for(int i:list){
-            System.out.println(i);
-        }
-        System.out.println("Triggered");
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+
     }
 
-    private class AsyncFetch extends AsyncTask<String, String, String>{
-        String signature="";
+    private class AsyncFetch extends AsyncTask<String, String, String> {
+        String signature = "";
+        boolean mode;
+
         @Override
         protected String doInBackground(String... strings) {
-
+            Log.i("mode",Boolean.toString(mode));
+            if (mode == false) {
                 //Linking Brute Algorithm to the Application
                 BruteForce.TripObject o = BruteForce.CallBrute(list, budget);
                 signature = o.getInfoPrint();
-               // testview.setText(signature);
-               // print();
-                publishProgress(signature);
+            } else {
 
-            return signature;
+                Log.i("TAG", "testtest");
+                //Fast algo
+                NearestNeighbour fastNN = new NearestNeighbour();
+                double myAmt = 20.0;
+                fastNN.setBudget(myAmt);
+
+                //convert ArrayList<Integer> to int[]
+                int[] ints = new int[list.size()];
+                for (int i = 0, len = list.size(); i < len; i++)
+                    ints[i] = list.get(i);
+//            int[] myIntArr = {1, 5, 4, 3};
+                fastNN.setDestination(ints);
+                fastNN.getModeOfTransport();
+                //System.out.println(fastNN);
+                signature = fastNN.getString();
+
+            }
+
+            return null;
         }
 
         @Override
         protected void onPostExecute(String s) {
-            //updatedData=s;
             testview.setText(signature);
         }
-
         @Override
         protected void onPreExecute() {
             testview.setText("Loading plans...");
+                mode=algo;
+
+            if (list.size()>=5 && algo==false){
+                mode=true;
+                Toast.makeText(context,"NOTICE:Fast algo has been activated to accomodate your large request",Toast.LENGTH_LONG).show();
+            }
         }
+
     }
-
-
 }
 
